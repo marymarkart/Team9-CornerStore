96568a1ac6b0ba548dd1778f9376c8da783b2349
Mary <mary.markart@sjsu.edu>
2022-05-15 13:58:47 -0700

    Edits to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,27 +6,27 @@ from myapp import myapp_obj
 
 @pytest.fixture()
 def app():
-    myapp = myapp_obj
-    myapp.config.update(
+    app = myapp_obj
+    app.config.update(
         TESTING=True,
         SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",  # in-memory test db
         WTF_CSRF_ENABLED=False  # disable CSRF for easy posting
     )
 
-    myapp.test_client_class = FlaskLoginClient
+    app.test_client_class = FlaskLoginClient
 
     # holds context of the app for testing
-    with myapp.app_context():
+    with app.app_context():
         # get the database object and create the tables inside the memory test-database
         from myapp import db
-        db.init_app(myapp)  # relink it to the app to get the updated uri
+        db.init_app(app)  # relink it to the app to get the updated uri
         db.create_all()
-    yield myapp
+    yield app
 
     # Can add cleanup here
 
 
 
 @pytest.fixture()
-def client(myapp):
-    return myapp.test_client()
+def client(app):
+    return app.test_client()
